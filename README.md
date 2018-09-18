578e5a6c3d9156822c61ed6506695f8198866bbb
Felix Mattick <felix.mattick@gmail.com>
2018-09-18 12:34:07 -0400

    Add setup script

--- README.md
+++ README.md
@@ -12,6 +12,11 @@ $ git clone https://github.com/bottza/bootbot.git
 $ cd bootbot
 $ npm install
 ```
+Next, go [here](https://discordapp.com/developers/applications/) and create a Discord application and make it a bot, if you haven't already. Copy the bot's token. Now run:
+```
+$ npm run setup
+```
+Enter your token and then copy your user ID in Discord by enabling developer mode, right clicking on your profile, and clicking `Copy ID`. Enter that in too. That's it!
 
 ## Running
 
---
+++ setup.js
@@ -0,0 +1,38 @@
+var prompts = require("prompts");
+var chalk = require("chalk");
+var fs = require("fs");
+
+(async () => {
+  var questions = [
+    {
+      type: "text",
+      name: "token",
+      message: "Your bot token",
+      validate: value => !value ? "Please enter a bot token!" : true
+    },
+    {
+      type: "number",
+      name: "owner",
+      message: "Your user id",
+      validate: value => !value ? "Please enter a user id!" : true
+    }
+  ];
+  var responses = await prompts(questions);
+  if (!responses.token || !responses.owner) {
+    console.log(chalk.red("Please enter values!"));
+  } else {
+    var string = "module.exports.token = \""
+               + responses.token + "\";\n"
+               + "module.exports.token = \""
+               + responses.owner + "\";";
+               var fs = require("fs");
+    fs.writeFile("config.js", string, function(error) {
+      if (error) {
+        console.log(chalk.red("Error in saving config!"));
+        console.log(chalk.red(error.message));
+      } else {
+        console.log(chalk.green("Done saving config!"));
+      }
+    }); 
+  }
+})();
