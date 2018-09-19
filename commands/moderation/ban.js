a6afbfbd57a6ddb0107a400be56d7e0827d1277d
Felix Mattick <felix.mattick@gmail.com>
2018-09-18 23:09:17 -0400

    Fix banning

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -7,37 +7,37 @@ module.exports = class ReplyCommand extends Command {
       group: "moderation",
       memberName: "ban",
       description: "Bans someone.",
-      examples: ["ban @User", "ban @User Reason.", "ban @User 5 Reason"],
+      examples: ["ban @User", "ban @User Reason.", "ban @User Reason 5"],
       userPermissions: ["BAN_MEMBERS"],
       clientPermissions: ["BAN_MEMBERS"],
       guildOnly: true,
       args: [
         {
-          key: "user",
+          key: "member",
           prompt: "What user do you want to ban?",
           type: "member"
-        },
-        {
-          key: "days",
-          prompt: "How many days should they be banned for?",
-          type: "integer",
-          default: 0
         },
         {
           key: "reason",
           prompt: "What is the reason for banning them?",
           type: "string",
           default: ""
+        },
+        {
+          key: "number",
+          prompt: "How many days should they be banned for?",
+          type: "integer",
+          default: 0
         }
       ]
     });
   }
 
-  run(msg, {member, days, reason}) {
+  run(msg, {member, number, reason}) {
     var daysMessage = "";
-    if (days > 0) {
+    if (number > 0) {
       daysMessage = " You will be unbanned in "
-        + days
+        + number
         + " days.";
     }
 
@@ -45,7 +45,7 @@ module.exports = class ReplyCommand extends Command {
     if (reason) {
       reasonMessage = " for \""
         + reason
-        + "\*.";
+        + "\".";
     }
 
     var message = "You have been banned from "
@@ -54,10 +54,10 @@ module.exports = class ReplyCommand extends Command {
       + daysMessage;
     member.send(message);
 
-    if (days <= 0) {
+    if (number <= 0) {
       member.ban(reason = reason);
     } else {
-      member.ban(days, reason);
+      member.ban(number, reason);
     }
 
     return msg.say("They were banned successfully.");
